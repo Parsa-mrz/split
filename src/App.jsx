802918c109a895604adff6194c337ec717ac3c06
Parsa Mirzaie <Mirzaie_parsa@protonmail.ch>
2025-05-09 01:18:50 +0330

    :sparkles: Add base friend list components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import FriendList from "./components/FriendList.jsx";
 export default function App() {
   return (
-    <>
-
-    </>
+    <div className='app'>
+      <div className='sidebar'>
+        <FriendList/>
+      </div>
+    </div>
   )
 }
